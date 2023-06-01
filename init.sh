1733c5e80a4b7888bdf4ceda5f72c08103db09be
adjarar <82608399+adjarar@users.noreply.github.com>
2023-06-01 04:54:56 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,8 +3,15 @@ MODELS_DIR="$ROOT_DIR/models"
 
 cd /home/webui
 
+# per instance ssh key
+cat ~/.ssh/authorized_keys | md5sum | awk '{print $1}' > ssh_key_hv; echo -n $VAST_CONTAINERLABEL | md5sum | awk '{print $1}' > instance_id_hv; head -c -1 -q ssh_key_hv instance_id_hv > ~/.vast_api_key;
+
 # item generation script
 git clone https://github.com/adjarar/rpg_img_generate.git
+
+# download the vastai script
+cd rpg_img_generate
+wget https://raw.githubusercontent.com/vast-ai/vast-python/master/vast.py -O vast; chmod +x vast;
 
 # copy webui settings
 git clone https://github.com/adjarar/webui-settings.git
