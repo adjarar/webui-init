1621ea91834afca8bc1adb86c6105acb20865215
adjarar <82608399+adjarar@users.noreply.github.com>
2023-06-02 17:58:49 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,11 @@
-ROOT_DIR="/home/webui/stable-diffusion-webui"
+ROOT_DIR="/stable-diffusion-webui"
 MODELS_DIR="$ROOT_DIR/models"
 
-cd /home/webui
 
 # per instance ssh key
 cat ~/.ssh/authorized_keys | md5sum | awk '{print $1}' > ssh_key_hv; echo -n $VAST_CONTAINERLABEL | md5sum | awk '{print $1}' > instance_id_hv; head -c -1 -q ssh_key_hv instance_id_hv > ~/.vast_api_key;
+
+cd $ROOT_DIR
 
 # item generation script
 git clone https://github.com/adjarar/rpg_img_generate.git
@@ -18,18 +19,6 @@ git clone https://github.com/adjarar/webui-settings.git
 mv -f webui-settings/* stable-diffusion-webui
 rm -rf webui-settings
 
-# Install extensions
-cd $ROOT_DIR/extensions
-git clone https://github.com/Coyote-A/ultimate-upscale-for-automatic1111.git 
-git clone https://github.com/Mikubill/sd-webui-controlnet.git
-git clone https://github.com/yfszzx/stable-diffusion-webui-images-browser.git
-git clone https://github.com/pkuliyi2015/multidiffusion-upscaler-for-automatic1111.git
-
-# VAE
-wget -q -P $MODELS_DIR/VAE \
-        https://huggingface.co/stabilityai/sd-vae-ft-ema-original/resolve/main/vae-ft-ema-560000-ema-pruned.ckpt \
-        https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.ckpt
-        
 # download SD models
 cd $MODELS_DIR/Stable-diffusion
 gdown https://civitai.com/api/download/models/67584 && \
